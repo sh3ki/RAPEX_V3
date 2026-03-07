5dbac8b2b4c9eea299b4a9a0f3f2a51ae5cb8e8f
sh3ki <shekaigarcia@gmail.com>
2026-03-07 21:54:46 +0800

    Create backend/apps/ready_to_eat/apps.py

---
+++ backend/apps/ready_to_eat/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+class ReadyToEatConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.ready_to_eat'
+    verbose_name = 'ReadyToEat'
+
