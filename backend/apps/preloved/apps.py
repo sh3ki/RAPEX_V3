c03c3fcaf5ac24b872cadc46886810a7f6f04f05
sh3ki <shekaigarcia@gmail.com>
2026-03-07 21:54:45 +0800

    Create backend/apps/preloved/apps.py

---
+++ backend/apps/preloved/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+class PrelovedConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.preloved'
+    verbose_name = 'Preloved'
+
