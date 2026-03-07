3653d3de77da6652f3f2acc51a96efa1186aa4f6
sh3ki <shekaigarcia@gmail.com>
2026-03-07 21:54:35 +0800

    Create backend/apps/fresh_market/apps.py

---
+++ backend/apps/fresh_market/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+class FreshMarketConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.fresh_market'
+    verbose_name = 'FreshMarket'
+
