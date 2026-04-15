54b68458af29afbc620b358f67818d0806db6b60
sh3ki <shekaigarcia@gmail.com>
2026-04-16 02:36:22 +0800

    Update frontend/rider/next.config.js

--- frontend/rider/next.config.js
+++ frontend/rider/next.config.js
@@ -1,3 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { env: { NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api/v1' } };
+const nextConfig = {
+	reactStrictMode: true,
+	experimental: {
+		externalDir: true,
+	},
+	env: {
+		NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api/v1',
+	},
+};
+
 module.exports = nextConfig;
